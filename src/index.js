aa82fca34074d416da7596afdf59aedd5e850163
Aleksey-Dr <aprel181818@gmail.com>
2023-04-09 06:53:19 +0300

    Add src html markup and update index.js
    Add src html markup and update index.js:
    1. Change language to "en".
    2. Add src html (gallery, load-more) to project:
    1) Add html file "gallery.html" to folder "partials";
    2) Add html file "load-more.html" to folder "partials";
    3) Add the markup (src html (gallery)) to the file "gallery.html";
    4) Add the markup (src html (load-more)) to the file "load-more.html";
    5) Include the "gallery.html" to the "index.html" in "src" folder;
    6) Include the "load-more.html" to the "index.html" in "src" folder.
    3. Add/find elements from html: Create object with elements.
    4. Add CONSTANTS

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// console.log('Hello!');
+
+// Add/find elements from html
+// Create object with elements
+const refs = {
+    searchForm: document.querySelector('.search-form'),
+    galleryImages: document.querySelector('.gallery'),
+    loadMoreBtn: document.querySelector('.load-more'),
+};
+// console.log(refs.searchForm);
+// console.log(refs.galleryImages);
+// console.log(refs.loadMoreBtn);
+
+// Add CONSTANTS
+// Personal key for Pixabay API
+const KEY_TO_API = '35129314-12d9f6cafbe4df38ad9bc5f6b';
